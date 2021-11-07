a70bb6acc66b836b653358490255676b5417f959
IndongLee <austen.lee@kakaocorp.com>
2021-11-07 23:36:48 +0900

    ssh key 적용 후 커밋 테스트

--- week 1-Jetpack Compose basics/ComposeBasics/app/src/main/java/com/indong/composebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeBasics/app/src/main/java/com/indong/composebasics/MainActivity.kt
@@ -44,7 +44,7 @@ import com.indong.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)  ㅂ
+        super.onCreate(savedInstanceState)
         setContent {
             ComposeBasicsTheme {
                 MyApp()
