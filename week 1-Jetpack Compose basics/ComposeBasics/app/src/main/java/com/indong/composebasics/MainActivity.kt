d95371141e3213efd6a111e9b916b9f280f854a8
austen.lee <austen.lee@kakaocorp.com>
2021-11-07 21:21:23 +0900

    week 1 - Text에 Modifier 추가.

--- week 1-Jetpack Compose basics/ComposeBasics/app/src/main/java/com/indong/composebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeBasics/app/src/main/java/com/indong/composebasics/MainActivity.kt
@@ -3,11 +3,15 @@ package com.indong.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.indong.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +31,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!")
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
     }
 }
 
