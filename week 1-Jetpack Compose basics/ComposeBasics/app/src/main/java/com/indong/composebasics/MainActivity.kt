2365aae5af20bcd1b2250e47d057e4e8a5af52af
austen.lee <austen.lee@kakaocorp.com>
2021-11-07 21:59:29 +0900

    week 1 - state 사용.

--- week 1-Jetpack Compose basics/ComposeBasics/app/src/main/java/com/indong/composebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeBasics/app/src/main/java/com/indong/composebasics/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,6 +45,8 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 private fun Greeting(name: String) {
+    val expended = remember { mutableStateOf(false) }
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -52,8 +56,8 @@ private fun Greeting(name: String) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            OutlinedButton(onClick = { expended.value = !expended.value }) {
+                Text(text = if (expended.value) "Show less" else "Show more")
             }
         }
     }
